ea30b5f5a941cc1b0757a6672d3219a6d76ce45b
lifeweaverlifeweaver-hash <lifeweaverlifeweaver@gmail.com>
2026-04-16 11:54:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfkaWtQyWDHL0IXCG4CrpAOX3Nt9pmcpE",
+  authDomain: "smartfermata.firebaseapp.com",
+  databaseURL: "https://smartfermata-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "smartfermata",
+  storageBucket: "smartfermata.firebasestorage.app",
+  messagingSenderId: "638532750076",
+  appId: "1:638532750076:web:2b1fa54869b120be586579",
+  measurementId: "G-SSVRM2WDM2"
+}
